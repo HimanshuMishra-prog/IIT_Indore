8e0e5eb989f3936f6a943e37f73863ae52ff6f71
HimanshuMishra-prog <himanshucode796@gmail.com>
2024-08-14 19:39:38 +0530

    Final Commit

--- Assignment 1/Question18.c
+++ Assignment 1/Question18.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(){
     char str1[100];
@@ -10,14 +11,20 @@ int main(){
     scanf("%99s", str1);
     printf("Enter the second string");
     scanf("%99s", str2);
-    int len = strlen(str1);
-    
-    if(strncmp(str1,str2, len) == 0){
-        printf("The strings are same");
-    }
-    else{
-        printf("The strings are different");
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    if(len1 != len2){
+         printf("Strings are not same");
+         return 0;
     }
 
+    for(int i = 0; i< len1 && i < len2 ; i++){
+        if(tolower(str1[i]) != tolower(str2[i])){
+            printf("Strings are not same");
+            return 0;
+        }
+    }
+    printf("Strings are same");
+    return 0;
 
 }
